f3273745ebcbe4cc0dfd92730afe69cd07d4fc5c
Mims H. Wright <mims@mimswright.com>
2023-01-27 09:33:43 +0100

    style(elastic): cleaned up elastic code

--- src/factory/elastic.ts
+++ src/factory/elastic.ts
@@ -1,3 +1,4 @@
+import { pipe } from "ramda";
 import { cubicOut } from "./../preset/exp";
 import { transitionWithControl } from "./../combinator/transition";
 import { max, forceStart0AndEnd1 } from "./../decorator/limit";
@@ -23,13 +24,12 @@ export const elastic =
   (frequency: number) =>
   (energy: number) =>
   (stiffness: number): EasingFunction =>
-    forceStart0AndEnd1(
+    pipe(
+      // generate the wave
+      () => sine(frequency)(energy),
+      // use stiffness value to dampen the wave converging onto 1
+      dampen(stiffness)(1),
       // Transition between starting point (0) and 1 based on the frequency of the wave
-      rampUpWaveFromZero(frequency)(
-        // use stiffness value to dampen the wave converging onto 1
-        dampen(stiffness)(1)(
-          // generate the wave
-          sine(frequency)(energy)
-        )
-      )
-    );
+      rampUpWaveFromZero(frequency),
+      forceStart0AndEnd1
+    )();
